38d97d9d19627cdf21c8883fde27771cf2413c61
TuanAnhKazuto <tuananhkazuto@gmail.com>
2024-11-27 10:27:47 +0700

    Setup HpBar && StaminaBar

--- Assets/_Scripts/Player/AtkCollider.cs
+++ Assets/_Scripts/Player/AtkCollider.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class AtkCollider : MonoBehaviour
 {
     public GameObject boxAttack;
+    [HideInInspector] public bool isAttacking;
 
     private void Awake()
     {
         boxAttack.SetActive(false);
+        isAttacking = false;
     }
     public void OnAtkCollider()
     {
         boxAttack.SetActive(true);
+        isAttacking = true;
     }
 
     public void OffAtkCollider()
     {
         boxAttack.SetActive(false);
+        isAttacking = false;
     }
 }

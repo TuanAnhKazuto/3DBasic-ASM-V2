fd451ad3236ee099fdefd1929b4cfb07c3b1ec9e
TuanAnhKazuto <tuananhkazuto@gmail.com>
2024-11-29 03:44:20 +0700

    edit tấn công, làm stamina(chưa xong)

--- Assets/_Scripts/Player/AtkCollider.cs
+++ Assets/_Scripts/Player/AtkCollider.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class AtkCollider : MonoBehaviour
 {
     public GameObject boxAttack;
-    [HideInInspector] public bool isAttacking;
+    //[HideInInspector] public bool isAttacking;
+    //[HideInInspector] public bool whenAttack;
 
     private void Awake()
     {
         boxAttack.SetActive(false);
-        isAttacking = false;
+        //isAttacking = false;
+        //whenAttack = false;
     }
     public void OnAtkCollider()
     {
         boxAttack.SetActive(true);
-        isAttacking = true;
+        //isAttacking = true;
+        //whenAttack = true;
+        //Invoke(nameof(StopBool), 0.1f);
     }
+
+    //public void StopBool ()
+    //{
+    //    whenAttack = false;
+    //}
 
     public void OffAtkCollider()
     {
         boxAttack.SetActive(false);
-        isAttacking = false;
+        //isAttacking = false;
     }
 }
--- Assets/_Scripts/Player/CharacterMovement.cs
+++ Assets/_Scripts/Player/CharacterMovement.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class CharacterMovement : MonoBehaviour
 {
@@ -13,15 +15,36 @@ public class CharacterMovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     [HideInInspector] public bool isRunning = false;
-
     public AtkCollider atkCollider;
-
-    public PlayerStamina stamina;
+    public Vector3 direction;
 
     // State 
-    [HideInInspector] public enum CharState
-    { Normal, Run, Attack, Die}
+    public enum CharState
+    { Normal, Run, Attack, Die }
     public CharState curState;
+    //bool isRun;
+
+    [Header("Attack")]
+    public int noOfClicks = 0;
+    float lastClickTime = 0;
+    public float maxCombooDelay = 0.9f;
+    public bool isAttacking = false;
+    bool hasSubStamina = false;
+
+    [Header("Status")]
+    public Slider staminaSlider;
+    [HideInInspector] public float maxStm = 100f;
+    public float curStamina;
+    
+    [HideInInspector] public float countReturn;
+
+
+    private void Awake()
+    {
+        maxStm = 100f;
+        curStamina = maxStm;
+        staminaSlider.value = curStamina;
+    }
 
     public void ChageState(CharState newState)
     {
@@ -42,15 +65,16 @@ public class CharacterMovement : MonoBehaviour
             case CharState.Normal:
                 animator.SetBool("Run", false);
                 animator.SetBool("Attack", false);
+                //isRun = false;
                 speed = 3f;
                 break;
             case CharState.Run:
-                if(stamina.curStamina < 3) return;
                 animator.SetBool("Run", true);
+                //isRun = true;
                 speed = 7f;
                 break;
             case CharState.Attack:
-                if(stamina.curStamina < 5) return;
+                if (curStamina < 7f) return;
                 animator.SetBool("Attack", true);
                 animator.SetFloat("Speed", 0f);
                 break;
@@ -77,15 +101,26 @@ public class CharacterMovement : MonoBehaviour
     private void Update()
     {
         Attack();
+        RecoveryStamina();
+        SubStaminaWhenRun();
+
+        if (curStamina <= 0)
+        {
+            curStamina = 0;
+        }
+        if(curStamina >= maxStm)
+        {
+            curStamina = maxStm;
+        }
     }
 
     private void Movement()
     {
-        if (atkCollider.isAttacking) return;
+        if (isAttacking) return;
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
@@ -96,9 +131,6 @@ public class CharacterMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
 
-            
-
-            // Chỉ chuyển sang trạng thái Run nếu đang không phải Run
             if (Input.GetMouseButton(1) && curState != CharState.Run)
             {
                 ChageState(CharState.Run);
@@ -110,7 +142,7 @@ public class CharacterMovement : MonoBehaviour
                 isRunning = false;
             }
         }
-        else if (curState != CharState.Normal) // Đứng yên -> Chuyển về trạng thái Normal
+        else if (curState != CharState.Normal) 
         {
             ChageState(CharState.Normal);
         }
@@ -118,16 +150,142 @@ public class CharacterMovement : MonoBehaviour
         animator.SetFloat("Speed", direction.magnitude);
     }
 
+    #region Code Attack
     void Attack()
     {
-        if (Input.GetMouseButton(0) && curState != CharState.Attack)
+        //if (Input.GetMouseButton(0) && curState != CharState.Attack)
+        //{
+        //    ChageState(CharState.Attack);
+        //}
+        //else if (!Input.GetMouseButton(0) && curState == CharState.Attack)
+        //{
+        //    ChageState(CharState.Normal);
+        //}
+
+        if(Time.time - lastClickTime > maxCombooDelay)
         {
-            ChageState(CharState.Attack);
+            noOfClicks = 0;
         }
-        else if (!Input.GetMouseButton(0) && curState == CharState.Attack)
+
+        if(Input.GetMouseButtonDown(0))
         {
-            ChageState(CharState.Normal);
+            lastClickTime = Time.time;
+            noOfClicks++;
+            if(noOfClicks == 1)
+            {
+                SubStaminaWhenAttack();
+                animator.SetBool("Attack01", true);
+
+            }
+            noOfClicks = Mathf.Clamp(noOfClicks, 0, 5);
         }
+
+        if(noOfClicks == 0)
+        {
+            isAttacking = false;
+        }
+
     }
 
+    public void EndAttack01()
+    {
+        if(noOfClicks >= 2)
+        {
+            SubStaminaWhenAttack();
+            hasSubStamina = true;
+
+            animator.SetBool("Attack02", true);
+            animator.SetBool("Attack01", false);
+
+        }
+        else
+        {
+            hasSubStamina = false;
+
+            animator.SetBool("Attack01", false);
+        }
+    }
+    public void EndAttack02()
+    {
+        if (noOfClicks >= 3)
+        {
+            SubStaminaWhenAttack();
+            hasSubStamina = true;
+
+            animator.SetBool("Attack03", true);
+            animator.SetBool("Attack02", false);
+        }
+        else
+        {
+            hasSubStamina = false;
+
+            animator.SetBool("Attack02", false);
+        }
+    }
+    public void EndAttack03()
+    {
+        if (noOfClicks >= 4)
+        {
+            SubStaminaWhenAttack();
+            hasSubStamina = true;
+
+            animator.SetBool("Attack04", true);
+            animator.SetBool("Attack03", false);
+
+        }
+        else
+        {
+            hasSubStamina = false;
+
+            animator.SetBool("Attack03", false);
+        }
+    }
+    public void EndAttack04()
+    {
+        isAttacking = false;
+        animator.SetBool("Attack01", false);
+        animator.SetBool("Attack02", false);
+        animator.SetBool("Attack03", false);
+        animator.SetBool("Attack04", false);
+        noOfClicks = 0;
+    }
+    #endregion
+
+    // Stamina
+
+    public void SubStaminaWhenRun()
+    {
+        if(curState == CharState.Run)
+        {
+            curStamina -= 3f * Time.deltaTime;
+            staminaSlider.value = curStamina;
+            //Debug.Log(direction.magnitude);     
+        }
+
+    }
+
+    public void SubStaminaWhenAttack()
+    {
+        curStamina -= 7f;
+        staminaSlider.value = curStamina;
+    }
+
+    public void RecoveryStamina()
+    {
+        if (isAttacking || curState == CharState.Run) return;
+        if(curStamina < maxStm)
+        {
+            curStamina += countReturn * Time.deltaTime;
+            staminaSlider.value = curStamina;
+        }
+
+        if (direction.magnitude <= 0f)
+        {
+            countReturn = 5;
+        }
+        else if(direction.magnitude > 0.1f)
+        {
+            countReturn = 3;
+        }
+    }
 }

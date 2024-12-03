f04aa442b9d75a29b0930331e42176711f4b3532
HuynhCuongg <huynhcuongvan2004@gmail.com>
2024-12-04 05:27:11 +0700

    sd

--- Assets/_Scripts/Item/InventoryManager.cs
+++ Assets/_Scripts/Item/InventoryManager.cs
@@ -7,7 +7,9 @@ using UnityEngine.UI;
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance { get; private set; }
+
     public List<Item> items = new List<Item>();
+
     public Transform itemContentPane;
     public GameObject itemPrefab;
 
@@ -24,10 +26,16 @@ public class InventoryManager : MonoBehaviour
     public void Add(Item item)
     {
         items.Add(item);
+        DisplayInventory();
     }    
 
     public void DisplayInventory()
     {
+        foreach (Transform item in itemContentPane)
+        {
+            Destroy(item);
+        }
+
         foreach (Item item in items)
         {
             GameObject obj = Instantiate(itemPrefab, itemContentPane);

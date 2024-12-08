bb5c0ac0bb539523b3b32181d563c4f3d6f9f839
HAUMEOOOO <haumdps40975@gmail.com>
2024-12-09 03:33:31 +0700

    Hau
    nút resume exit hoạt động

---
+++ Assets/_Scripts/Player/EXIT.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExitToMainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        // Tìm nút EXIT bằng tag 'exit' và gán sự kiện
+        GameObject exitButton = GameObject.FindGameObjectWithTag("exit");
+        if (exitButton != null)
+        {
+            Button button = exitButton.GetComponent<Button>();
+            button.onClick.AddListener(LoadMainMenu);
+        }
+        else
+        {
+            Debug.LogWarning("Không tìm thấy nút với tag 'exit'.");
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        // Tải Scene MainMenu
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("Loading MainMenu...");
+    }
+}
